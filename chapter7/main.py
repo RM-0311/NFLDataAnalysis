eb28f74e2c745d851b4d67ea14a02f47884bd46d
unknown <md.ridgeway.work@gmail.com>
2025-02-06 15:44:06 -0600

    Charting Draft Effectiveness

--- chapter7/main.py
+++ chapter7/main.py
@@ -30,4 +30,22 @@ draft.to_csv("draft.csv", index=False)
 
 draft_use = draft[["Season", "Pick", "Tm", "Player", "Pos", "wAV", "DrAV"]]
 
-print(draft_use)
+sns.set_theme(style="whitegrid", palette="colorblind")
+draft_use_pre2020 = draft_use.query("Season <= 2020")
+draft_use_pre2020 = draft_use_pre2020.astype({"Pick": int, "DrAV": float})
+# sns.regplot(data=draft_use_pre2020,
+#             x="Pick",
+#             y="DrAV",
+#             line_kws={"color": "red"},
+#             scatter_kws={"alpha": 0.2})
+
+draft_chart = draft_use_pre2020.groupby(["Pick"]).agg({"DrAV": ["mean"]})
+draft_chart.columns = list(map("_".join, draft_chart.columns))
+draft_chart.loc[draft_chart.DrAV_mean.isnull()] = 0
+draft_chart["roll_DrAV"] = (
+  draft_chart["DrAV_mean"]
+  .rolling(window=13)
+  .mean()
+  )
+sns.scatterplot(draft_chart, x="Pick", y="roll_DrAV", color="red")
+plt.show()
