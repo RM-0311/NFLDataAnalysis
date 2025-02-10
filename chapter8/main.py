19a596f059a198a35ff753e361f3429617eeae5c
unknown <md.ridgeway.work@gmail.com>
2025-02-10 14:32:39 -0600

    Pull Combine Data

--- chapter8/main.py
+++ chapter8/main.py
@@ -10,3 +10,25 @@ for i in range(2000, 2024 + 1):
   web_data = pd.read_html(url)[0]
   web_data["Season"] = i
   web_data = web_data.query('Ht != "Ht"')
+  combine = pd.concat([combine, web_data])
+
+combine.reset_index(drop=True, inplace=True)
+combine.to_csv("combine_data.csv", index=False)
+
+combine[["Ht_ft", "Ht_in"]] = combine["Ht"].str.split("-", expand=True)
+combine = combine.astype({
+  "Wt": float,
+  "40yd": float,
+  "Vertical": float,
+  "Bench": float,
+  "Broad Jump": float,
+  "3Cone": float,
+  "Shuttle": float,
+  "Ht_ft": float,
+  "Ht_in": float
+})
+
+combine["Ht"] = (combine["Ht_ft"] * 12.0 + combine["Ht_in"])
+
+combine.drop(["Ht_ft", "Ht_in"], axis=1, inplace=True)
+print(combine.describe())
